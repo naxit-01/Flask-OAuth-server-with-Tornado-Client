de8e3767644c0ba2e0ba249a33ffeeaa33dfa0a7
naxit-01 <bzatek@scg.cz>
2021-04-21 13:58:25 +0200

    verze 4

---
+++ verze 4/Projekt OAuth/AuthSer2/website/oauth2.py
@@ -0,0 +1,119 @@
+from authlib.integrations.flask_oauth2 import (
+    AuthorizationServer,
+    ResourceProtector,
+)
+from authlib.integrations.sqla_oauth2 import (
+    create_query_client_func,
+    create_save_token_func,
+    create_revocation_endpoint,
+    create_bearer_token_validator,
+)
+from authlib.oauth2.rfc6749 import grants
+from authlib.oauth2.rfc7636 import CodeChallenge
+from .models import db, User
+from .models import OAuth2Client, OAuth2AuthorizationCode, OAuth2Token
+
+
+class AuthorizationCodeGrant(grants.AuthorizationCodeGrant):
+    print("CLASS authorizationcodegrant")
+    TOKEN_ENDPOINT_AUTH_METHODS = [
+        'client_secret_basic',
+        'client_secret_post',
+        'none',
+    ]
+
+    def save_authorization_code(self, code, request):
+        print("DEF save_authorization_code")
+        code_challenge = request.data.get('code_challenge')
+        code_challenge_method = request.data.get('code_challenge_method')
+        auth_code = OAuth2AuthorizationCode(
+            code=code,
+            client_id=request.client.client_id,
+            redirect_uri=request.redirect_uri,
+            scope=request.scope,
+            user_id=request.user.id,
+            code_challenge=code_challenge,
+            code_challenge_method=code_challenge_method,
+        )
+        db.session.add(auth_code)
+        db.session.commit()
+        return auth_code
+
+    def query_authorization_code(self, code, client):
+        print("DEF query_authorization_code")
+        auth_code = OAuth2AuthorizationCode.query.filter_by(
+            code=code, client_id=client.client_id).first()
+        if auth_code and not auth_code.is_expired():
+            return auth_code
+
+    def delete_authorization_code(self, authorization_code):
+        print("DEF delete_authorization_code")
+        db.session.delete(authorization_code)
+        db.session.commit()
+        
+    def delete_access_token(self, accessToken):
+        print("DEF delete_access_token")
+        db.session.delete(accessToken)
+        de.session.commit()
+
+    def authenticate_user(self, authorization_code):
+        print("DEF authenticate_user")
+        return User.query.get(authorization_code.user_id)
+
+    
+
+class PasswordGrant(grants.ResourceOwnerPasswordCredentialsGrant):
+    #nebezpecny zpusob predavani - nepouzivat
+    def authenticate_user(self, username, password):
+        print("DEF authenticate_user")
+        user = User.query.filter_by(username=username).first()
+        if user is not None and user.check_password(password):
+            return user
+
+
+class RefreshTokenGrant(grants.RefreshTokenGrant):
+    def authenticate_refresh_token(self, refresh_token):
+        print("DEF authenticate_refresh_token")
+        token = OAuth2Token.query.filter_by(refresh_token=refresh_token).first()
+        if token and token.is_refresh_token_active():
+            return token
+
+    def authenticate_user(self, credential):
+        print("DEF authenticate_user")
+        return User.query.get(credential.user_id)
+
+    def revoke_old_credential(self, credential):
+        print("DEF revoke_old_credential")
+        credential.revoked = True
+        db.session.add(credential)
+        db.session.commit()
+
+
+print("oauth2.py: BODY")
+query_client = create_query_client_func(db.session, OAuth2Client)
+save_token = create_save_token_func(db.session, OAuth2Token)
+authorization = AuthorizationServer(
+    query_client=query_client,
+    save_token=save_token,
+)
+require_oauth = ResourceProtector()
+
+
+def config_oauth(app):
+    print("DEF config_oauth")
+    authorization.init_app(app)
+
+    # support all grants
+    authorization.register_grant(grants.ImplicitGrant)
+    authorization.register_grant(grants.ClientCredentialsGrant)
+    authorization.register_grant(AuthorizationCodeGrant, [CodeChallenge(required=True)])
+    authorization.register_grant(PasswordGrant)
+    authorization.register_grant(RefreshTokenGrant)
+
+    # support revocation
+    revocation_cls = create_revocation_endpoint(db.session, OAuth2Token)
+    authorization.register_endpoint(revocation_cls)
+
+    # protect resource
+    bearer_cls = create_bearer_token_validator(db.session, OAuth2Token)
+    require_oauth.register_token_validator(bearer_cls())
